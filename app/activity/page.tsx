c4bf09e9feec4cfab8198913269ceedd48a407fb
Pkmmte Xeleon <hi@pkmmte.com>
2025-11-26 00:18:08 -0800

    feat: activity page

---
+++ app/activity/page.tsx
@@ -0,0 +1,8 @@
+export default function ActivityPage() {
+	return (
+		<div className="flex items-center justify-center min-h-screen">
+			<p className="text-center">Activity Page</p>
+		</div>
+	)
+}
+
